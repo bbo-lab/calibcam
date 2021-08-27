5f336acfe497ecc731fe4d6b79929f07719ed047
Kay-Michael Voit <kay-michael.voit@caesar.de>
2021-08-27 16:25:08 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import find_packages, setup
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="bbo-calibcam",
+    version="1.1.0",
+    description="Calibrate intrinsic and extrinsic parameters of cameras with charuco boards",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bbo-lab/calibcam",
+    author="BBO-lab @ caesar",
+    author_email="kay-michael.voit@caesar.de",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=['calibcam'],
+    include_package_data=True,
+    install_requires=["matplotlib", "numpy", "opencv", "py-opencv", "pyyaml", "scipy", "bbo-ccvtools", "pyqt5", "autograd"],
+)
