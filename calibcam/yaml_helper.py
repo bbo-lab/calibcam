cb759f3a9842a99f18b6167428707a6e450843fa
cheekoti_la <abhilash.cheekoti@mpinb.mpg.de>
2024-03-05 18:18:32 +0100

    yaml functions moved to calibcamlib

--- calibcam/yaml_helper.py
+++ calibcam/yaml_helper.py
@@ -1,26 +1,12 @@
 import numpy as np
 from copy import deepcopy
+from calibcam import calibrator_opts
+from calibcamlib.yaml_helper import *
+
 
 def get_calib_numpy_fields():
     return ['rvec_cam', 'tvec_cam', 'A', 'xi', 'k', 'rvecs', 'tvecs', 'frames_mask', 'std_intrinsics',
             'std_extrinsics', 'per_view_errors']
-
-
-def numpy_collection_to_list(collection):
-    if isinstance(collection, np.ndarray):
-        return collection.tolist()
-    if not isinstance(collection, dict) and not isinstance(collection, list):
-        return deepcopy(collection)
-    collection = deepcopy(collection)
-    for k in collection:
-        if k in collection:
-            if isinstance(collection[k], np.ndarray):
-                collection[k] = collection[k].tolist()
-            elif isinstance(collection[k], dict):
-                collection[k] = numpy_collection_to_list(collection[k])
-            elif isinstance(collection[k], list):
-                collection[k] = [numpy_collection_to_list(le) for le in collection[k]]
-    return collection
 
 
 def load_calib(calib):
@@ -32,9 +18,10 @@ def load_calib(calib):
 
 
 def load_opts(opts):
-    # TODO: Implement recursive conversion and limit fields to those that should actually be converted!
     opts = deepcopy(opts)
-    for k in opts.keys():
+    default_opts = calibrator_opts.get_default_opts()
+    for k in default_opts.keys():
         if k in opts:
-            opts[k] = np.array(opts[k])
+            opts[k] = collection_to_array(opts[k])
+
     return opts
